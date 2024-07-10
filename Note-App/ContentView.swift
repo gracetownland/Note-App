de7ea44057d21abe00b80f67a9a47ad9d3e49d21
gracetownland <speak2ayushsrihari@gmail.com>
2024-07-10 16:38:46 -0700

    Initial Commit

---
+++ Note-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note-App
+//
+//  Created by Ayush S on 2024-07-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note-App/Note_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_AppApp.swift
+//  Note-App
+//
+//  Created by Ayush S on 2024-07-10.
+//
+
+import SwiftUI
+
+@main
+struct Note_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
